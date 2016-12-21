9c2742ccfb90fbc14814035604de891dff17c15f
liuhongliang <752284118@qq.com>
2016-12-21 14:10:29 +0800

    添加当前item是否是当前这种viewType的结尾或者开头

--- library/src/main/java/me/drakeet/multitype/ItemViewProvider.java
+++ library/src/main/java/me/drakeet/multitype/ItemViewProvider.java
@@ -62,4 +62,39 @@ public abstract class ItemViewProvider<T, V extends ViewHolder> {
     protected List<?> getItems() {
         return items;
     }
+    /**
+     * 判断当前item是否是当前这种viewType的开头
+     * @param t 当前数据类型
+     * @return true：是这种viewType的开头，否则false
+     * @since v2.3.2
+     */
+    protected boolean isViewTypeStart(T t){
+        int position = getPosition();
+        if(position==0){//已经是顶部了
+            return true;
+        }
+        Object preViewTypeBean = getItems().get(position - 1);//上一个类型
+        if(t!=null&&preViewTypeBean!=null&&!t.getClass().getName().equals(preViewTypeBean.getClass().getName())){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 判断当前item是否是当前这种viewType的结尾
+     * @param t 当前数据类型
+     * @return true：是这种viewType的结尾，否则false
+     * @since v2.3.2
+     */
+    protected boolean isViewTypeEnd(T t){
+        int position = getPosition();
+        if(position+1==items.size()){//已经是尾部了
+            return true;
+        }
+        Object nextViewTypeBean = getItems().get(position + 1);//上一个类型
+        if(t!=null&&nextViewTypeBean!=null&&!t.getClass().getName().equals(nextViewTypeBean.getClass().getName())){
+            return true;
+        }
+        return false;
+    }
 }
